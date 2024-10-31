c7d10f14cf64fd93b167a967aabef122ceb71838
Natalya Botsman <botsmannatashaa@gmail.com>
2024-10-31 14:56:06 +0600

    Created app Routing and displayed ToolBar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
+import ToolBar from './Components/ToolBar/ToolBar.tsx';
+import { Route, Routes } from 'react-router-dom';
+import Content from './Containers/Content/Content.tsx';
+import './App.css';
+import Admin from './Containers/Admin/Admin.tsx';
+
 const App = () => {
-  return <></>;
+  return <>
+    <ToolBar/>
+    <div className="container my-5">
+      <Routes>
+        <Route path="/" element={<Content/>}/>
+        <Route path="/pages/:pageName" element={<Content/>}/>
+        <Route path="/pages/admin" element={<Admin/>}/>
+        <Route path="*" element={<h1>Not found</h1>}/>
+      </Routes>
+    </div>
+  </>;
 };
 
 export default App;
